18584341165460bde78abde20499fb724827acae
Paco Ho <kaiipho@gmail.com>
2024-04-30 10:41:26 +0800

    add camel cases functions

--- src/PkgExt.jl
+++ src/PkgExt.jl
@@ -4,6 +4,8 @@ import Pkg
 # Write your package code here.
 
 export all_pkgs, install_deps
+export allPkgs, installDeps, setPyEnv
+
 
 function all_pkgs()
     deps = Pkg.dependencies()
@@ -37,5 +39,39 @@ function set_py_env(curdir = pwd())
     ENV["PYCALL_JL_RUNTIME_PYTHON"] = path
     Pkg.build("PyCall")
 end
+
+function allPkgs()
+    deps = Pkg.dependencies()
+    installs = Dict{String, VersionNumber}()
+    for (_, dep) in deps
+        dep.is_direct_dep || continue
+        dep.version === nothing && continue
+        installs[dep.name] = dep.version::VersionNumber
+    end
+    return installs
+end
+
+function installDeps(pkgs::AbstractVector{String})
+    installedpackages = allPkgs()
+    for pkg in pkgs
+        if !haskey(installedpackages, pkg)
+            println("Package '$pkg' is not installed. Installing...")
+            Pkg.add(pkg)
+        end
+    end
+end
+
+function setPyEnv(curdir = pwd())
+    Pkg.activate(curdir)
+    haskey(allPkgs(), "PyCall") || Pkg.add("PyCall")
+
+    # give the path of python executable
+    path = Sys.iswindows() ? joinpath(curdir, "Scripts", "python.exe") : joinpath(curdir, "bin", "python")
+
+    ENV["PYTHON"] = path
+    ENV["PYCALL_JL_RUNTIME_PYTHON"] = path
+    Pkg.build("PyCall")
+end
+
 
 end
