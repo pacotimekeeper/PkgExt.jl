5bbaace6b382181871a89f793aae20ad43891717
pacotimekeeper <kaiipho@gmail.com>
2026-06-21 23:33:02 +0800

    update some function

--- src/PkgExt.jl
+++ src/PkgExt.jl
@@ -3,11 +3,10 @@ module PkgExt
 import Pkg
 # Write your package code here.
 
-export all_pkgs, install_deps
-export allPkgs, installDeps, setPyEnv
-export pipInstall, pipInstalled, pipList
+export allpkgs, installdep, setpyenv
+export pipinstall, pipinstalled, piplist
 
-function all_pkgs()
+function allpkgs()
     deps = Pkg.dependencies()
     installs = Dict{String, VersionNumber}()
     for (_, dep) in deps
@@ -18,8 +17,8 @@ function all_pkgs()
     return installs
 end
 
-function install_deps(pkgs::AbstractVector{String})
-    installedpackages = all_pkgs()
+function installdep(pkgs::AbstractVector{String})
+    installedpackages = allpkgs()
     for pkg in pkgs
         if !haskey(installedpackages, pkg)
             println("Package '$pkg' is not installed. Installing...")
@@ -28,9 +27,9 @@ function install_deps(pkgs::AbstractVector{String})
     end
 end
 
-function set_py_env(curdir = pwd())
+function setpyenv(curdir = pwd())
     Pkg.activate(curdir)
-    haskey(all_pkgs(), "PyCall") || Pkg.add("PyCall")
+    haskey(allpkgs(), "PyCall") || Pkg.add("PyCall")
 
     # give the path of python executable
     path = Sys.iswindows() ? joinpath(curdir, "Scripts", "python.exe") : joinpath(curdir, "bin", "python")
@@ -40,7 +39,7 @@ function set_py_env(curdir = pwd())
     Pkg.build("PyCall")
 end
 
-function allDependencies()
+function alldependencies()
     deps = Pkg.dependencies()
     installs = Dict{String, VersionNumber}()
     for (_, dep) in deps
@@ -51,52 +50,15 @@ function allDependencies()
     return installs
 end
 
-
-## CamelCase functions
-function allPkgs()
-    deps = Pkg.dependencies()
-    installs = Dict{String, VersionNumber}()
-    for (_, dep) in deps
-        dep.is_direct_dep || continue
-        dep.version === nothing && continue
-        installs[dep.name] = dep.version::VersionNumber
-    end
-    return installs
-end
-
-function
-
-function installDeps(pkgs::AbstractVector{String})
-    installedpackages = allPkgs()
-    for pkg in pkgs
-        if !haskey(installedpackages, pkg)
-            println("Package '$pkg' is not installed. Installing...")
-            Pkg.add(pkg)
-        end
-    end
-end
-
-function setPyEnv(curdir = pwd())
-    Pkg.activate(curdir)
-    haskey(allPkgs(), "PyCall") || Pkg.add("PyCall")
-
-    # give the path of python executable
-    path = Sys.iswindows() ? joinpath(curdir, "Scripts", "python.exe") : joinpath(curdir, "bin", "python")
-
-    ENV["PYTHON"] = path
-    ENV["PYCALL_JL_RUNTIME_PYTHON"] = path
-    Pkg.build("PyCall")
-end
-
-function pipInstall(pkg::String)
+function pipinstall(pkg::String)
     run(`pip install $pkg`)
 end
 
-function activatePyEnv()
+function activatepyenv()
     run(`source bin activate`)
 end
 
-function pipList()
+function piplist()
     io = IOBuffer()
     cmd = pipeline(`pip list`; stdout=io, stderr=devnull)
     run(cmd)
@@ -115,6 +77,6 @@ function pipList()
     # end
 end
 
-pipInstalled(pkg::String) = haskey(pipList(), "xlrd")
+pipinstalled(pkg::String) = haskey(piplist(), "xlrd")
 
 end
